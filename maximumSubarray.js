a3810198b2ebaa474fbb1e38d3518a7c1da32083
Stefan <stefanleoussis@gmail.com>
2022-05-11 09:50:56 -0400

    maximumSubarray

---
+++ maximumSubarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ *
+ * Easy - May 10th 2022
+ */
+const maxSubArray = function (nums) {
+  if (!nums.length) {
+    return [];
+  }
+
+  return;
+};
+
+console.log(maxSubArray([]));
